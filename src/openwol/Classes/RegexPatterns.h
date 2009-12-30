2f797267bed75e3ada22584bb17095d908a1820d
iceboundrock <iceboundrock@gmail.com>
2009-12-30 08:19:22 +0000

    add #ifndef

--- src/openwol/Classes/RegexPatterns.h
+++ src/openwol/Classes/RegexPatterns.h
@@ -6,6 +6,10 @@
 //  Copyright 2009 Lychee Studio. All rights reserved.
 //
 
-
+#ifndef IP_PATTERN
 #define IP_PATTERN  (@"^\\s*(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\s*$")
+#endif
+
+#ifndef MAC_PATTERN
 #define MAC_PATTERN  (@"^\\s*([0-9A-Fa-f]{2})-([0-9A-Fa-f]{2})-([0-9A-Fa-f]{2})-([0-9A-Fa-f]{2})-([0-9A-Fa-f]{2})-([0-9A-Fa-f]{2})\\s*$")
+#endif
